2cac34a2aa6b868160216223b29f5556c78259b9
Benjamin Jeran <108634496+Oblakoru@users.noreply.github.com>
2025-05-25 11:44:23 +0200

    Dodane barve za  stiliranje

---
+++ lib/colors/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Colors
+  static const Color leafGreen = Color(0xFF7CB518);
+  static const Color oliveGreen = Color(0xFF5C8014);
+  static const Color lime = Color(0xFFB8E986);
+  static const Color paleLime = Color(0xFFE9FFDF);
+
+  // Secondary Colors
+  static const Color tomatoRed = Color(0xFFF03A47);
+
+  // Neutral Colors
+  static const Color softCream = Color.fromARGB(255, 255, 255, 255);
+  static const Color paleGray = Color(0xFFEAEAEA);
+  static const Color charcoal = Color(0xFF333333);
+  static const Color dimGray = Color(0xFF686868);
+  static const Color white = Color(0xFFFFFFFF);
+}
