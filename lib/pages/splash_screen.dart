7d26959bef2748aa44e8dd271cbc5a97368a579f
Benjamin Jeran <108634496+Oblakoru@users.noreply.github.com>
2025-05-31 09:18:11 +0200

    Ce si prijavljen, skippa login screen ob startu appa

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,8 +1,10 @@
 // lib/pages/splash_screen.dart
 import 'package:dish_dash/pages/auth/login_screen.dart';
+import 'package:dish_dash/pages/main_tab_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:dish_dash/pages/onboarding_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -20,35 +22,46 @@ class _SplashScreenState extends State<SplashScreen>
   void initState() {
     super.initState();
 
-    // Animation setup
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 1), // How long 1 full bob takes
-    )..repeat(reverse: true); // Repeats up and down
+      duration: const Duration(seconds: 1),
+    )..repeat(reverse: true);
 
     _animation = Tween<Offset>(
       begin: const Offset(0, 0),
-      end: const Offset(0, -0.05), // Slight upward motion
+      end: const Offset(0, -0.05),
     ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
-    _checkOnboardingStatus();
+
+    _initializeApp();
   }
 
-  Future<void> _checkOnboardingStatus() async {
+  bool _checkUserStatus() {
+    return FirebaseAuth.instance.currentUser != null;
+  }
+
+  Future<void> _initializeApp() async {
     final prefs = await SharedPreferences.getInstance();
     final bool hasSeenOnboarding = prefs.getBool('hasSeenOnboarding') ?? false;
 
     Future.delayed(const Duration(seconds: 2), () {
-      if (hasSeenOnboarding) {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => const LoginScreen()),
-        );
-      } else {
+      if (!hasSeenOnboarding) {
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (context) => const OnboardingScreen()),
         );
         prefs.setBool('hasSeenOnboarding', true);
+      } else {
+        if (_checkUserStatus()) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const MainTabScreen()),
+          );
+        } else {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const LoginScreen()),
+          );
+        }
       }
     });
   }
